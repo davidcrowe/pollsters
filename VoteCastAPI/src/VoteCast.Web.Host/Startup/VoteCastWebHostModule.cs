edb93dbe8a7f95dfe48f2ac09132878e55740d57
SMImrulKabir <86272713+SMImrulKabir@users.noreply.github.com>
2021-07-01 00:36:27 -0400

    Add files via upload

---
+++ VoteCastAPI/src/VoteCast.Web.Host/Startup/VoteCastWebHostModule.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Abp.Modules;
+using Abp.Reflection.Extensions;
+using VoteCast.Configuration;
+
+namespace VoteCast.Web.Host.Startup
+{
+    [DependsOn(
+       typeof(VoteCastWebCoreModule))]
+    public class VoteCastWebHostModule: AbpModule
+    {
+        private readonly IWebHostEnvironment _env;
+        private readonly IConfigurationRoot _appConfiguration;
+
+        public VoteCastWebHostModule(IWebHostEnvironment env)
+        {
+            _env = env;
+            _appConfiguration = env.GetAppConfiguration();
+        }
+
+        public override void Initialize()
+        {
+            IocManager.RegisterAssemblyByConvention(typeof(VoteCastWebHostModule).GetAssembly());
+        }
+    }
+}
